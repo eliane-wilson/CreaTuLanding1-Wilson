cdbd2ef92c7c4198ff1d3dd8362069d0dc6e4c62
Eliane <elianewilson2024@gmail.com>
2025-08-25 18:48:18 -0300

    crea tu landing + Wilson

--- src/components/CartWidget.jsx
+++ src/components/CartWidget.jsx
@@ -1,11 +1,8 @@
 function CartWidget() {
   return (
     <button className="btn btn-outline-light position-relative"> 
-    🛒
-      <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
-        0
-
-      </span>
+    🛒 Carrito
+   
     </button>
   )
 }
